7fbb8837ff571258024f1f2708e7b2b848d5c5ba
Abhishek Mondal <71382408+AbhishekMondal1@users.noreply.github.com>
2021-12-26 15:36:56 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
+const { DefaultAzureCredential } = require("@azure/identity");
+const { SecretClient } = require("@azure/keyvault-secrets");
 const app = require('express')();
 const port =8080; 
+const keyVaultName = 'the-matrix-kv' // process.env["VAULT_NAME"];
+   const KVUri = "https://" + keyVaultName + ".vault.azure.net";
 
+   const credential = new DefaultAzureCredential();
+   const client = new SecretClient(KVUri, credential);
    
-
+   const secretName = "MyPass";
    app.get('/api/test', async(req, res) => {
- 
+   const secret = await getSecret();
 
       res.type('text');
       res.send('secret');
@@ -16,6 +22,10 @@ const port =8080;
    app.get('/tst', (req,res)=>{
       res.send('tst');
       })
+async function getSecret() {
+      const retrievedSecret = await client.getSecret(secretName);
+      return retrievedSecret;
+   }
 
 
    app.listen(port, () => {
